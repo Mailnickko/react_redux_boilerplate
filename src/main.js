53647ae2a318e064c5acb196ee0194d917e268fe
Nicholas Ko <mailnickko@gmail.com>
2017-01-06 23:33:41 -0800

    (feat) Configure entry point for webpack

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// From dependencies
+import { render } from 'react-dom';
+import React from 'react';
+import { Router } from 'react-router';
+import { Provider } from 'react-redux';
+
+import { store, history } from './store/configureStore';
+
+// From App
+import './styles/styles.css';
+import rootReducer from './reducers';
+import routes from './routes';
+
+const router = (
+  <Provider store={store}>
+    <Router history={history} >
+      {routes}
+    </Router>
+  </Provider>
+);
+
+render(router, document.querySelector('#app'));
